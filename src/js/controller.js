28d0299c3095d60cd76ffa3e9ff2021fce803b81
Dennis-7 <dashingdennis7@gmail.com>
2022-07-07 16:10:55 +0000

    Add new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -131,6 +131,10 @@ const controlAddRecipe = async function (newRecipe) {
   // Upload the new recipe data
 };
 
+const newFeature = function () {
+  console.log('Welcome to new feature branch');
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -139,6 +143,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  console.log('Welcome');
+  newFeature();
 };
 init();
