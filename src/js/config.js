e8f8e43b64de7acd25c42f032757ba8bf4618efc
Dennis-7 <dashingdennis7@gmail.com>
2022-07-07 14:28:23 +0000

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 15;
+export const KEY = '43d94f96-16cf-43e5-9603-c145276ee5d3';
+export const MODAL_CLOSE_SEC = 2.5;
